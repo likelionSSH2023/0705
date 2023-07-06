db2942afeb406fabc84d95da074383ba5a1f3459
심서현 <simseohyun@simseohyeon-ui-MacBookAir.local>
2023-07-06 11:13:29 +0900

    first commit

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,14 @@
+// App.jsx
+import React from 'react';
+import FakeNews from './components/FakeNews';
+
+
+function App() {
+  return (
+    <>
+    <FakeNews/>
+    </>
+  );
+}
+
+export default App;
---
+++ vite-project/src/components/useContext/ContextComponent.jsx
@@ -0,0 +1,12 @@
+// ContextComponent.jsx
+import React from 'react';
+import Child from './Child';
+
+export default function ContextComponent() {
+  return (
+    <div>
+      내 이름은....
+      <Child />
+    </div>
+  );
+}
